1c363ccf87a9b300b52e81553878334f1c4be0e7
nflores101 <neil.flores@macquarie.com>
2016-08-31 18:23:49 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,11 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
+  file { '/etc/motd':
+      ensure => fiile,
+      owner  => 'root',
+      group  => 'root'
+      mode   => '0644'
+      content => "Wow! I know now how to manage!\n",
+      }
 }
