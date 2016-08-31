8781e30135f4bfc239fbf36306a7263543aa8892
nflores101 <neil.flores@macquarie.com>
2016-08-31 19:34:03 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -52,7 +52,7 @@ node default {
       #}
   
   exec { "cowsay 'Welcome to $(::fqdn)!' > /etc/motd":
-      path => '/usr/local/bin'
+      path => '/usr/local/bin',
       creates => '/etc/motd',
       }
 }
